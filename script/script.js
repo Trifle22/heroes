4310cc94bbd49476509d712297cd7614607fc2df
Trifle22 <xromchenkov99@gmail.com>
2021-03-14 14:06:11 +0300

    +invalid search

--- script/script.js
+++ script/script.js
@@ -1,19 +1,16 @@
-
 const request = new XMLHttpRequest();
-
 request.open('GET', './dbHeroes.json');
-
 request.send();
-
 request.addEventListener('readystatechange', (event) => {
   if (request.readyState === 4) {
     showHeroes(JSON.parse(request.response))
   }
 })
 
+const heroesBlock = document.querySelector('.heroes-cards')
+
 function showHeroes(heroes) {
   console.log(heroes);
-  const heroesBlock = document.querySelector('.heroes-cards')
   heroes.forEach(hero => {
     const heroCard = `
     <div class="hero-card">
@@ -21,13 +18,50 @@ function showHeroes(heroes) {
     <span class="hero-name">${hero.name}</span>
     <span class="hero-live-name">${hero.actors}</span>
     <span class="hero-status">${hero.status}</span>
+    <span class="hero-films">${hero.movies ? hero.movies.join(', ') : ''}</span>
     </div>`;
     heroesBlock.insertAdjacentHTML('beforeend', heroCard);
-    const filmListElement = `<ul></ul>`
-    hero.movies.forEach(item => {
-      const filmElement = `<li>${item}</li>`
-      filmListElement.appendChild(filmElement);
-    })
-    heroesBlock.insertAdjacentHTML('beforeend', filmListElement)
+  })
+}
+
+const searchInput = document.querySelector('.search');
+
+const debounce = (func, ms) => {
+  let timeout;
+  return function () {
+    const fnCall = () => func.apply(this, arguments)
+    clearTimeout(timeout);
+    timeout = setTimeout(fnCall, ms);
+  }
+}
+
+function searchFilms(target) {
+  fetch('./dbHeroes.json')
+    .then(response => response.json())
+    .then(response => showFoundFilms(response, target))
+}
+
+searchFilms = debounce(searchFilms, 300);
+
+searchInput.addEventListener('input', (event) => {
+  searchFilms(event.target);
+});
+
+function showFoundFilms(response, target) {
+  const val = target.value;
+  response.forEach(item => {
+    if (item.movies && item.movies.join(', ').toLowerCase().includes(val)) {
+      console.log(1);
+      heroesBlock.textContent = '';
+      const heroCard = `
+      <div class="hero-card">
+      <img src="${item.photo}" alt="hero-avatar" class="hero-avatar">
+      <span class="hero-name">${item.name}</span>
+      <span class="hero-live-name">${item.actors}</span>
+      <span class="hero-status">${item.status}</span>
+      <span class="hero-films">${item.movies ? item.movies.join(', ') : ''}</span>
+      </div>`;
+      heroesBlock.insertAdjacentHTML('beforeend', heroCard);
+    }
   })
 }
